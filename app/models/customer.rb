fe74561bb82002f4d171e682106f7162f9791a1e
Maldon <maldonmeehan@gmail.com>
2016-11-08 15:00:48 -0800

    add validation to model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,9 @@
 class Customer < ApplicationRecord
+  
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence:true, length: {minimum: 2}
+
+  def address
+	   "#{street} #{zip} #{city}"
+  end
 end
