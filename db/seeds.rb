ac5a56ff287757adf52310aa6a6bb681e990f3e2
Maldon <maldonmeehan@gmail.com>
2016-11-08 14:59:25 -0800

    create and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Customer.destroy_all
+
+
+Customer.create!([
+    {
+    	id: 1, first_name: "John", last_name: "Doe", email: "john@doe.com",
+        street: "Allen St.", city: "Manhattan", zip: "10002"
+     },
+     {
+    	id: 2, first_name: "Jane", last_name: "Roe", email: "jane@roe.com",
+        street: "Boo St.", city: "Kirkland", zip: "98034"
+    },
+    {
+    	id: 3, first_name: "Foo", last_name: "Bar", email: "foo@bar.com",
+        street: "Boo St.", city: "Kirkland", zip: "98034"
+    },
+    {
+    	id: 4, first_name: "Rachid", last_name: "Benkirane", email: "contact@roe.com",
+        street: "Boo St.", city: "Kirkland",zip: "98034"
+    }
+ ])
+
+p "Created #{Customer.count} customers."
